94413ec9f4290bf38efa18696e1f41e56a35d402
KingJeremyNg <jeremy97ng@gmail.com>
2019-09-18 08:44:26 -0400

    Added number reverseNumbers function

---
+++ reverseNumbers.py
@@ -0,0 +1,19 @@
+def reverse(number) :
+    count = 1
+    digits = 1
+    temp = number
+
+    while temp > 0 :
+        temp -= (temp % (10 ** digits))
+        if temp == 0 :
+            break
+        digits += 1
+
+    newNum = 0
+    while digits >= 0 :
+        newNum += (number % (10 ** count)) * (10 ** (digits - count))
+        number -= (number % (10 ** count))
+        count += 1
+        digits -= 1
+
+    return newNum
